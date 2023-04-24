51d4678e5f8e8b641ca3b1b3a31b862f041caa57
Agustin Rezett <agustin.rezett@gmail.com>
2023-04-24 11:32:38 -0300

    fix: vercel analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,11 +2,13 @@ import '../styles/globals.css';
 import { Provider } from 'react-redux';
 import store from '../redux/store';
 import withRedux from 'next-redux-wrapper';
+import { Analytics } from '@vercel/analytics/react';
 
 function App({ Component, pageProps }) {
 	return (
 		<Provider store={store}>
 			<Component {...pageProps} />
+			<Analytics />
 		</Provider>
 	);
 }
